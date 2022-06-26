eddf52d1de8c8543fbe84fc32af73c4ca422bb5b
jingjingxyk <zonghengbaihe521@qq.com>
2022-06-26 18:28:07 +0800

    改进

--- content-scripts/wubi-assistant-search.js
+++ content-scripts/wubi-assistant-search.js
@@ -13,6 +13,21 @@ let init = () => {
         let input_text_box = document.querySelector('#textfield');
         if (input_text_box) {
             input_text_box.setAttribute('value', keyword)
+            let input_verify_code=document.querySelector('#textfield2')
+            if(input_verify_code)
+            {
+                input_verify_code.addEventListener('keydown',event=>{
+                    if(event.keyCode=== 13)
+                    {
+                        let button=document.querySelector('#button')
+                        if(button)
+                        {
+                            button.click();
+                        }
+                        event.preventDefault();
+                    }
+                })
+            }
         }
     }
 

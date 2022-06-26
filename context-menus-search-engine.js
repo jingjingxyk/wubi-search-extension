aa7416aadf27fff9bde0cac7bb439889785a69e3
jingjingxyk <zonghengbaihe521@qq.com>
2022-06-26 19:22:27 +0800

    add wubit query site

--- context-menus-search-engine.js
+++ context-menus-search-engine.js
@@ -43,7 +43,17 @@ let context_menu_search_engin = () => {
      //http://hy.httpcn.com/html/wubi/26/PWUYCQAZILTBPWCQAZ/
      // https://www.4qx.net/m/wubi_results.php
 
+     // https://search.cidianwang.com/?m=0&y=0&t=3&q=%E4%BA%94%E7%AC%94%E6%9F%A5%E8%AF%A2
 
+     // http://www.daziba.cn/tools/wbcx.aspx
+
+    // https://www.iamwawa.cn/wubi.html
+    //https://www.qqxiuzi.cn/bianma/wubi.php
+    // https://qq.ip138.com/wb/wb.asp
+
+    //https://www.fanwenzhan.com/wubi/6KGo.shtml
+
+    //https://wubizhijia.com/e/search/result/?searchid=2644
 
 }
 
